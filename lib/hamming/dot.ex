87fa8c485cf6a0373763218e897c443679fa3397
Jonathan D. Storm <jonathan.dale.storm@protonmail.ch>
2015-06-26 23:42:52 -0500

    Updated dependencies

--- lib/hamming/dot.ex
+++ lib/hamming/dot.ex
@@ -1,25 +1,27 @@
 defmodule Hamming.Dot do
+  alias Math.Binary
+
   defp edge_to_dot({p, q}) do
     "#{p} -- #{q};\n"
   end
 
   @spec hamming_cube_edges(pos_integer) :: list
   def hamming_cube_edges(dimensions \\ 4) do
-    max_num = BitMath.pow_2(dimensions) - 1
+    max_num = Binary.pow_2(dimensions) - 1
 
     (for p <- 0..max_num,
-         q <- BitMath.hamming_shell(p, 1, dimensions) do
+         q <- Binary.hamming_shell(p, 1, dimensions) do
       {min(p, q), max(p, q)}
     end) |> Enum.uniq
   end
 
   @spec hamming_sphere_edges(integer, pos_integer, pos_integer) :: list
   def hamming_sphere_edges(center, radius, dimensions) do
-    sphere = BitMath.hamming_sphere(center, radius, dimensions)
+    sphere = Binary.hamming_sphere(center, radius, dimensions)
 
     (for p <- sphere,
          q <- sphere,
-         BitMath.hamming_distance(p, q) == 1 do
+         Binary.hamming_distance(p, q) == 1 do
       {min(p, q), max(p, q)}
     end) |> Enum.uniq
   end
@@ -34,20 +36,21 @@ defmodule Hamming.Dot do
     """
 
     for node <- (nodes |> Enum.sort) do
-      color = BitMath.hamming_distance(0, node) + 1
+      color = Binary.hamming_distance(0, node) + 1
       IO.puts("  #{node} [style=filled fillcolor=#{color}];")
     end
 
     edges
-      |> Enum.sort
-      |> Enum.map(&(edge_to_dot &1))
-      |> IO.puts
+    |> Enum.sort
+    |> Enum.map(&(edge_to_dot &1))
+    |> IO.puts
+
     IO.puts "}"
   end
 
   @spec print_hamming_sphere_graph(integer, pos_integer, pos_integer) :: String.t
   def print_hamming_sphere_graph(center, radius, dimensions) do
-    nodes = BitMath.hamming_sphere(center, radius, dimensions)
+    nodes = Binary.hamming_sphere(center, radius, dimensions)
     edges = hamming_sphere_edges(center, radius, dimensions)
 
     print_graph(nodes, edges)
@@ -55,8 +58,8 @@ defmodule Hamming.Dot do
 
   @spec print_hamming_cube_graph(pos_integer) :: String.t
   def print_hamming_cube_graph(dimensions) do
-    nodes = BitMath.hamming_cube(dimensions)
-    edges = hamming_cube_edges(center, radius, dimensions)
+    nodes = Binary.hamming_cube(dimensions)
+    edges = hamming_cube_edges(dimensions)
 
     print_graph(nodes, edges)
   end
